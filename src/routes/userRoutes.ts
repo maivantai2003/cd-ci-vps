c0228c1eec9420868052ba3bdcb81350f54a869c
Van Tai <vantai08122003@gmail.com>
2025-10-30 19:03:55 +0700

    chore(UserController):change scope access api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,11 +8,11 @@ const userRouter = Router();
 const userRepo=new UserRepository();
 const userService=new UserService(userRepo);
 const userController = new UserController(userService);
-userRouter.use(authMiddleware);
-userRouter.get("/", (req: Request, res: Response, next: NextFunction) =>
+//userRouter.use(authMiddleware);
+userRouter.get("/", authMiddleware,(req: Request, res: Response, next: NextFunction) =>
   userController.getAllUsers(req, res)
 );
-userRouter.get("/:id", (req: Request, res: Response, next: NextFunction) =>
+userRouter.get("/:id",authMiddleware, (req: Request, res: Response, next: NextFunction) =>
   userController.getUserById(req, res)
 );
 userRouter.post("/", (req: Request, res: Response, next: NextFunction) =>
